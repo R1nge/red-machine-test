962b60df664a51b940cd0c3cda972294ed6f1f26
R1nge <r1ngegamedev@gmail.com>
2024-02-28 15:15:37 +0300

    Базовое перемещение камеры при зажатой ЛКМ с помощью мышки

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Events;
+﻿using Events;
 using Player;
 using UnityEngine;
 using Utils.Singleton;
@@ -9,15 +8,51 @@ namespace Camera
     public class CameraMovement : DontDestroyMonoBehaviourSingleton<CameraMovement>
     {
         [SerializeField] private float moveSpeed;
+        private Vector3 _lastMousePosition;
+        private bool _dragging;
 
-
-        private void Update()
+        private void Start()
         {
-            if (PlayerController.PlayerState != PlayerState.Scrolling)
+            EventsController.Subscribe<EventModels.Game.PlayerFingerPlaced>(this, OnDragStart);
+            EventsController.Subscribe<EventModels.Game.PlayerFingerRemoved>(this, OnDragEnd);
+        }
+        
+        private void OnDestroy()
+        {
+            EventsController.Unsubscribe<EventModels.Game.PlayerFingerPlaced>(OnDragStart);
+            EventsController.Unsubscribe<EventModels.Game.PlayerFingerRemoved>(OnDragEnd);
+        }
+
+        private void OnDragStart(EventModels.Game.PlayerFingerPlaced e)
+        {
+            if(PlayerController.PlayerState != PlayerState.Scrolling)
                 return;
             
-            var movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), .0f);
-            CameraHolder.Instance.MainCamera.transform.Translate(movement * (moveSpeed * Time.deltaTime));
+            _lastMousePosition = CameraHolder.Instance.MainCamera.ScreenToWorldPoint(Input.mousePosition);
+            _dragging = true;
+        }
+        
+        private void OnDragEnd(EventModels.Game.PlayerFingerRemoved e)
+        {
+            _dragging = false;
+        }
+
+        private void LateUpdate()
+        {
+            if (_dragging)
+            {
+                var mousePosition = CameraHolder.Instance.MainCamera.ScreenToWorldPoint(Input.mousePosition);
+                var deltaPosition = _lastMousePosition - mousePosition; // This is the change in position
+                
+                var cameraTransform = CameraHolder.Instance.MainCamera.transform;
+                var newPosition = Vector3.Lerp(
+                    cameraTransform.position,
+                    cameraTransform.position - deltaPosition,
+                    moveSpeed);
+
+                CameraHolder.Instance.MainCamera.transform.position = newPosition;
+                _lastMousePosition = mousePosition;
+            }
         }
     }
 }
